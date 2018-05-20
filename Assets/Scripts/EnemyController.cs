8160348aa3e2396f677a701ed9b2c80e58a95689
Taigrrzi <tombatchelor12@yahoo.co.uk>
2018-05-20 20:44:43 +0100

    General Enemy Controller
    Filled out first details for the general enemy controller, to be inherited from by each enemy type.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public string displayName;
+    public string displayDescription;
+    public int initialHealth;
+    public enum aiStates {};
+
+    public int currentHealth;
+    public int initialState;
+
+    // Use this for initialization
+    void Start () {
+        currentHealth = initialHealth;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        UpdateState();
 	}
+
+    public virtual void EnterState() //Call when entering an AI State
+    {
+
+    }
+
+    public virtual void UpdateState() //Call every frame
+    {
+
+    }
+
+    public virtual void ExitState() //Call when entering an AI State
+    {
+
+    }
+
+    public virtual void Die() //Call when this enemy dies
+    {
+
+    }
+
+    public virtual void TakeDamage(int amount) //Call when this enemy takes damage
+    {
+        this.currentHealth -= amount;
+        if (currentHealth <= 0) {
+            this.Die();
+        }
+    }
 }
--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour {
 
-	// Use this for initialization
+	// Use this for initialization 
 	void Start () {
 		
 	}
